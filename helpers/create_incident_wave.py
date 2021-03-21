1187a3df6f05adb0dfa5a2621e0355491b4767e1
Arwin Verhoeven <arwin_verhoeven@hotmail.com>
2021-03-21 14:02:06 +0100

    First implementation of the algorithm using dynamic sample sizes

--- helpers/create_incident_wave.py
+++ helpers/create_incident_wave.py
@@ -6,7 +6,8 @@ Created on Mon Feb 15 11:19:20 2021
 @author: Arwin
 """
 import numpy as np
-from scipy.constants import speed_of_light
+from scipy.constants import speed_of_light, mu_0, epsilon_0
+from scipy.special import jv
 
 def create_planewave(planesize, grid_distance, E_0, wavelength, incident_angle, epsilon_B=1):
     """
@@ -47,3 +48,46 @@ def create_planewave(planesize, grid_distance, E_0, wavelength, incident_angle, 
     # Determine TM mode E-field
     E = E_0*np.exp(1j*np.dot(rho, k_B))
     return E
+
+def create_planewave_dynamic(locations, E_0, wavelength, incident_angle, epsilon_B=1):
+    """
+    Calculates the indident plane wave on specific locations. Used then using
+    a dynamic grid.
+
+    Parameters
+    ----------
+    locations : array
+        Array with coordinates where the plane wave has to be found.
+    E_0 : float
+        Amplitude of the plane wave.
+    wavelength : float
+        Wavelength of the plane wave.
+    incident_angle : float
+        Angle in radians of the incoming wave. The angle is defined as the
+        amount of radian from the vertical axis in counter-clockwise direction.
+    epsilon_B : float, optional
+        Relative permittivity of the background. The default is 1.
+
+    Returns
+    -------
+    E : numpy array
+        Nummpy array with amplitudes of the plane wave on the given locations.
+
+    """
+    modes = 50
+    omega = 2*np.pi*speed_of_light/wavelength #2*pi*f
+    k_0 = omega*np.sqrt(mu_0*epsilon_0)
+    r = np.linalg.norm(locations, axis=1)
+    theta = []
+    for l in locations:
+        theta.append(np.arctan2(l[1],l[0]))
+    theta = np.array(theta)
+    theta_i = incident_angle
+    E = np.zeros(np.shape(locations)[0], complex)  #allocating space for E-field
+    for i in range(0,2*modes+1):
+        n = i-modes
+        E = E + E_0*1j**n*jv(n,k_0*r)*np.exp(1j*n*(theta-theta_i))
+    return E
+
+
+
---
+++ martin98.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 18 14:25:03 2021
+
+@author: Arwin
+"""
+
+import numpy as np
+from scipy.constants import speed_of_light, mu_0, epsilon_0
+from scipy.special import hankel1
+from scipy.spatial.distance import pdist, squareform
+
+def martin98(locations, E_incident, permittivity, location_sizes, wavelength, step_size):
+    """
+    Basic implementation of the algorithm as proposed in [Olivier J. F. Martin
+    and Nicolas B. Piller, Electromagnetic scattering in polarizable 
+    back-grounds].
+
+    Parameters
+    ----------
+    locations : numpy array
+        Array containing the locations where the E field must be evaluated.
+    E_incident : numpy array
+        Array containing the value of the incident E field at each location.
+    permittivity : numpy array
+        Array containing the permittivity at each location.
+    location_sizes : numpy array
+        Array containing the size of the samples at each location.
+    wavelength : float
+        Wavelength of the incident wave.
+    step_size : float
+        Minimal distance between samples.
+
+    Returns
+    -------
+    E_r : numpy array
+        Scattered E field at each location.
+
+    """
+    # Find number of locations
+    nloc = np.shape(locations)[0]
+    # Relative permittivity of background
+    epsilon_B = 1
+    # Wave number
+    #k_0 = 2*np.pi*frequency/speed_of_light
+    k_0 = 2*np.pi/wavelength
+    k_rho = k_0*np.sqrt(epsilon_B)
+    # Calculate distance between all points in the plane
+    varrho = squareform(pdist(locations, 'euclidean'))
+    # Calculate G matrix
+    G_condensed = 1j/4*hankel1(0,k_rho*varrho)
+    # Convert condensed G matrix to square form
+    #G = squareform(G_condensed)
+    G = G_condensed
+    # Volume of each location
+    V_mesh = np.square(location_sizes*step_size)
+    # Self contribution to the electric field
+    R_eff = np.sqrt(V_mesh/np.pi) #Effective radius
+    beta = 1 # No coupling between TE and TM polarizations
+    gamma = R_eff/k_rho*hankel1(1, k_rho*R_eff) + 2j/(np.pi*np.square(k_rho))
+    M = 1j*np.pi/2*beta*gamma
+    # Set diagonal of G matrix to 0
+    np.fill_diagonal(G, M/V_mesh)
+    
+    # Difference between background permittivity and permittivity at a specific
+    # location
+    Delta_epsilon = permittivity - epsilon_B
+    
+    # Total E field (vector)
+    E_r = np.linalg.inv(np.identity(nloc) - k_0**2 * G @ np.diag(Delta_epsilon*V_mesh)) @ E_incident
+    return E_r
