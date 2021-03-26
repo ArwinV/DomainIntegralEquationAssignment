92067f16b085c50a8bdab6b504b273cb070b3ff7
Jules-Hilkens <64689330+Jules-Hilkens@users.noreply.github.com>
2021-03-26 12:04:40 +0100

    Update test_martin98.py
    Ff calculation with plot

--- tests/test_martin98.py
+++ tests/test_martin98.py
@@ -17,17 +17,20 @@ from martin98 import dynamic_shaping
 
 plane_size = (100,100)
 step_size = 10
-farfield_samples = 30 #Define how many ff samples you want
-ff_distance = 500 #Distance farfield out of grip
-r_ff = plane_size[0]*step_size+ff_distance #extra size from grid to ff
+farfield_samples = 120 #Define how many ff samples you want
+
 
 # Define input wave properties
 frequency = 1e6
 wavelength = speed_of_light/frequency
 input_angle = 120*np.pi/180
+diameter_cylinder = 50
 
 # Create a plane with a circle in the middle
-epsilon_circle = plane_with_circle(plane_size, step_size, 50, 4.7)
+epsilon_circle = plane_with_circle(plane_size, step_size, diameter_cylinder, 4.7)
+
+size_cylinder = np.pi*0.5*diameter_cylinder #Approximated size of cylinder working as transmitting area, is the circumference of the cylinder
+ff_distance = 2*size_cylinder**2/wavelength #Distance farfield out of grip
 
 # Convert grid to dynamic
 max_size = 4
@@ -38,12 +41,14 @@ loc_ff = []
 
 if (farfield_samples != 0):
     # TODO: Find farfield samples
-    ff_angle = np.linspace(input_angle+np.pi/8, input_angle+np.pi*9/8, farfield_samples)  #Starting angle in radians 45 degrees from incident
+    ff_angle = np.linspace(input_angle+np.pi/8, input_angle+np.pi*17/8-2*np.pi/farfield_samples, farfield_samples)  #Starting angle in radians 45 degrees from incident
     for k in range(farfield_samples):
         # y_ff = np.sin(ff_angle)*r_ff
         # x_ff = np.cos(ff_angle)*r_ff
-        loc_ff = [np.cos(ff_angle)*r_ff,np.sin(ff_angle)*r_ff]
+        loc_ff = [np.cos(ff_angle)*ff_distance,np.sin(ff_angle)*ff_distance]
         # loc_ff[1] = np.sin(ff_angle)*r_ff
+
+# loc_ff = plane_size[0]*step_size/2+loc_ff #extra size from grid to ff
 
 loc_ff = np.transpose(np.reshape(loc_ff,(2,farfield_samples)))
 loc_ff = loc_ff+(plane_size[0]/2*step_size)
@@ -73,7 +78,7 @@ simparams = {
 
 
 E_grid, E_ff = dynamic_shaping(simparams)
-# E_ff = E_ff/r_ff #Emplying the 1/r dependence
+E_ff = E_ff/ff_distance #Employing the 1/r dependence
 # E_ff = dynamic_shaping(simparams_ff)
 show_plane(np.absolute(E_grid.T), step_size, title="E field of algorithm solution")
 show_plane_ff(np.absolute(E_ff), loc_ff, title="Locations farfield of algorithm solution")
