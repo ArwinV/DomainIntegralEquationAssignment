3360f122be7c7f972fc7e53ea7f61069800af45d
Jules-Hilkens <64689330+Jules-Hilkens@users.noreply.github.com>
2021-03-30 11:49:49 +0200

    Final code changes

---
+++ Final/Main_domain_IE_v2.py
@@ -0,0 +1,218 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 27 15:27:14 2021
+
+@author: Arwin, Wendy
+"""
+
+import numpy as np
+from scipy.constants import speed_of_light
+from helpers.create_testobject import plane_with_circle
+from helpers.visualize import show_plane, show_plane_ff
+from domain_integral_equation import domain_integral_equation
+from helpers.calculate_error import energybased_error
+from validation.TEcil import Analytical_2D_TE
+from timeit import default_timer as timer
+from helpers.dynamic_grid import grid_to_dynamic, dynamic_to_grid
+from Final.martin98 import dynamic_shaping
+
+""""
+Define paramaters 
+"""
+# Create epsilon plane
+simulation_size = (60,60)
+total_size = 500;
+step_size = total_size/simulation_size[0]  #meters
+
+# Define input wave properties
+frequency = 1e6
+wavelength = speed_of_light/frequency
+theta_i = int(input("What is the angle of incidence? (Enter in degrees) \n"))
+input_angle = theta_i*np.pi/180
+
+q_static = (input("Do you want to calculate and plot the E field for the static grid? [Y/N] \n"))
+q_dynamic = (input("Do you want to calculate and plot the E field for the dynamic grid? [Y/N]\n"))
+farfield_samples = int(input("How many farfield samples do you want? If none then enter 0:\n"))
+
+"""
+Calculation and plot of the simulated grid with the cylinder
+""" 
+# Circle in middle
+circle_diameter = total_size/10 #meters
+circle_permittivity = 4.7 #relative (glass)
+epsilon_circle = plane_with_circle(simulation_size, step_size, circle_diameter, circle_permittivity)
+
+# Show plane
+show_plane(epsilon_circle, step_size, title="Plane on which the field is incident",plottype='epsilon')
+
+
+
+"""
+Calculation and plotting of E-field with a static grid
+""" 
+if (q_static == "N" or q_static == "n" or q_static == "No" or q_static == "no"):
+    print("No E-field will be calculated for the static grid")
+elif (q_static == "Y" or q_static == "y" or q_static == "Yes" or q_static == "yes"):
+    #STATIC GRID
+    # Store necessary variables into dictionary for E-field computation
+    simparams = {
+        'simulation_size': simulation_size,
+        'step_size': step_size,
+        'wavelength': wavelength,
+        'input_angle': input_angle,
+        'relative_permittivity': epsilon_circle,
+        'method': 'plane'
+        }
+    
+    # Compute E-field using domain_integral_equation
+    start_algorithm = timer()
+    E_field = domain_integral_equation(simparams).T
+    end_algorithm = timer()
+    print("Solution found with algorithm in {} seconds".format(end_algorithm-start_algorithm))
+    
+    # Show the calculated E field
+    show_plane(np.absolute(E_field), step_size, title="E field calculated with algorithm",plottype='fieldnorm')
+    
+    
+    
+    """
+    Reference code for the static grid
+    """
+    #REFERENCE STATIC GRID
+    # TEcil expects different simparams, so create new dictionary
+    xmin = -simulation_size[0]*step_size/2
+    xmax = simulation_size[0]*step_size/2
+    ymin = -simulation_size[1]*step_size/2
+    ymax = simulation_size[1]*step_size/2
+    xpoints,ypoints = np.meshgrid(np.linspace(xmin, xmax, simulation_size[0]), np.linspace(ymin, ymax, simulation_size[1]))
+    simparams = {
+        'frequency': frequency,
+        'radius': circle_diameter/2,
+        'epsilon_r': circle_permittivity,
+        'incident_angle': input_angle,
+        'modes': 50, #used in jupyter notebook example
+        'evaluation_points_x': xpoints,
+        'evaluation_points_y': ypoints
+        }
+    
+    # Compute E-field using TEcil
+    start_analytical = timer()
+    _, _, E_fieldval, E_inval = Analytical_2D_TE(simparams)
+    end_analytical = timer()
+    print("Analytical solution found in {} seconds".format(end_analytical-start_analytical))
+    
+    # Show the validation E field
+    show_plane(np.absolute(E_fieldval), step_size, title="E field of analytical solution on static grid")
+
+
+
+    """
+    Error Calculation
+    """
+    # Calculate difference in magnitude between implementation and validation
+    E_difference = np.abs(E_fieldval) - np.abs(E_field)
+    # Get the error between analytical and algorithm in percentage
+    E_error = np.abs(E_difference)/np.abs(E_fieldval) * 100
+    
+    E_error_abs, E_error_norm = energybased_error(E_fieldval,E_field)
+    
+    # Plot the error
+    show_plane(E_error, step_size, title="Error between analytical and static algorithm")
+
+else:
+    print("Incorrect input, E-field for the static grid will not be calculated")
+
+
+"""
+Calculation and plotting of E-field with a dynamic grid
+""" 
+if (q_dynamic == "N" or q_dynamic == "n" or q_dynamic == "No" or q_dynamic == "no"):
+    print("No E-field will be calculated for the dynamic grid")
+elif (q_dynamic == "Y" or q_dynamic == "y" or q_dynamic == "Yes" or q_dynamic == "yes"):
+    #DYNAMIC GRID
+    # Define dynamic grid properties
+    max_size = 4
+    size_limits = [0, max_size/2*circle_diameter, max_size*circle_diameter]
+    locations, location_sizes, epsilon = grid_to_dynamic(epsilon_circle, step_size, max_size, size_limits)
+    
+    size_circle = np.pi*0.5*circle_diameter #Approximated size of cylinder working as transmitting area, is the circumference of the cylinder
+    ff_begin = 2*size_circle**2/wavelength #Distance farfield out of grip
+    
+    simparams = {
+        'simulation_size': simulation_size,
+        'step_size': step_size,
+        'wavelength': wavelength,
+        'input_angle': input_angle,
+        'relative_permittivity': epsilon_circle,
+        'method': 'plane'
+        }
+    
+    simparams['relative_permittivity'] = epsilon
+    simparams['locations'] = locations
+    simparams['location_sizes'] = location_sizes
+    
+    start_dynamic = timer()
+    E_grid,E_ff = dynamic_shaping(simparams,farfield_samples)
+    end_dynamic = timer()
+    print("Solution found with dynamic algorithm in {} seconds".format(end_dynamic-start_dynamic))
+    
+    # Show the calculated E field
+    show_plane(np.absolute(E_grid), step_size, title="E field calculated with dynamic algorithm")
+
+
+
+    """
+    Reference code for the dynamic grid 
+    """
+    #REFERENCE DYNAMIC GRID
+    # TEcil expects different simparams, so create new dictionary
+    locations, location_sizes, epsilon = grid_to_dynamic(epsilon_circle, step_size, max_size, size_limits)
+    xpoints = locations[:,0] - simulation_size[0]*step_size/2
+    ypoints = locations[:,1] - simulation_size[1]*step_size/2
+    simparams = {
+        'relative_permittivity': epsilon,
+        'locations': locations,
+        'location_sizes': location_sizes,
+        'frequency': frequency,
+        'radius': circle_diameter/2,
+        'epsilon_r': circle_permittivity,
+        'incident_angle': input_angle,
+        'modes': 50, #used in jupyter notebook example
+        'evaluation_points_x': xpoints,
+        'evaluation_points_y': ypoints
+        }
+    
+    # Compute E-field using TEcil
+    start_analytical = timer()
+    _, _, E_fieldval_dyn, E_inval = Analytical_2D_TE(simparams)
+    end_analytical = timer()
+    print("Analytical solution found in {} seconds".format(end_analytical-start_analytical))
+    
+    # Show the validation E field
+    E_fieldval_dyn = dynamic_to_grid(locations,E_fieldval_dyn,location_sizes,simulation_size,step_size, 0)
+    show_plane(np.absolute(E_fieldval_dyn), step_size, title="E field of analytical solution on dynamic grid")    
+
+    
+    
+    """
+    Error Calculation
+    """
+    # # Calculate difference in magnitude between implementation and validation
+    E_difference_grid = np.abs(E_fieldval_dyn) - np.abs(E_grid)
+    # # Get the error between analytical and algorithm in percentage
+    E_griderror = np.abs(E_difference_grid)/np.abs(E_fieldval_dyn) * 100
+    
+    E_griderror_abs, E_griderror_norm = energybased_error(E_fieldval_dyn,E_grid)
+    
+    # # Plot the error
+    show_plane(E_griderror, step_size, title="Error between analytical and dynamic algorithm")
+    
+else:
+    print("Incorrect input, E-field for the dynamic grid will not be calculated")
+
+
+
+# Incident wave
+#E_in = dynamic_to_grid(locations,E_inval,location_sizes,simulation_size,step_size, farfield_samples).T
+#show_plane(np.real(E_in.T), step_size, title="Validation incident field")
+
--- Final/martin98.py
+++ Final/martin98.py
@@ -14,9 +14,65 @@ from helpers.visualize import show_plane,show_plane_ff
 from scipy.constants import epsilon_0, mu_0, speed_of_light
 from helpers.create_incident_wave import create_planewave_dynamic
 
-# def ff_samples(farfield_samples,step_size,simulation_size,ff_distance):
+def martin98(locations, E_incident, permittivity, location_sizes, wavelength, step_size):
+    """
+    Basic implementation of the algorithm as proposed in [Olivier J. F. Martin
+    and Nicolas B. Piller, Electromagnetic scattering in polarizable 
+    back-grounds].
 
+    Parameters
+    ----------
+    locations : numpy array
+        Array containing the locations where the E field must be evaluated.
+    E_incident : numpy array
+        Array containing the value of the incident E field at each location.
+    permittivity : numpy array
+        Array containing the permittivity at each location.
+    location_sizes : numpy array
+        Array containing the size of the samples at each location.
+    wavelength : float
+        Wavelength of the incident wave.
+    step_size : float
+        Minimal distance between samples.
 
+    Returns
+    -------
+    E_r : numpy array
+        Scattered E field at each location.
+
+    """
+    # Find number of locations
+    nloc = np.shape(locations)[0]
+    # Relative permittivity of background
+    epsilon_B = 1
+    # Wave number
+    #k_0 = 2*np.pi*frequency/speed_of_light
+    k_0 = 2*np.pi/wavelength
+    k_rho = k_0*np.sqrt(epsilon_B)
+    # Calculate distance between all points in the plane
+    varrho = squareform(pdist(locations, 'euclidean'))
+    # Calculate G matrix
+    G_condensed = 1j/4*hankel1(0,k_rho*varrho)
+    # Convert condensed G matrix to square form
+    #G = squareform(G_condensed)
+    G = G_condensed
+    # Volume of each location
+    V_mesh = np.square(location_sizes*step_size)
+    # Self contribution to the electric field
+    R_eff = np.sqrt(V_mesh/np.pi) #Effective radius
+    beta = 1 # No coupling between TE and TM polarizations
+    gamma = R_eff/k_rho*hankel1(1, k_rho*R_eff) + 2j/(np.pi*np.square(k_rho))
+    M = 1j*np.pi/2*beta*gamma
+    # Set diagonal of G matrix to 0
+    np.fill_diagonal(G, M/V_mesh)
+    
+    # Difference between background permittivity and permittivity at a specific
+    # location
+    Delta_epsilon = permittivity - epsilon_B
+    
+    # Total E field (vector)
+    E_r = np.linalg.inv(np.identity(nloc) - k_0**2 * G @ np.diag(Delta_epsilon*V_mesh)) @ E_incident
+    return E_r
 
 def dynamic_shaping(simparams, farfield_samples):
     #Initialization: read parameters from dictionary
@@ -92,63 +148,3 @@ def dynamic_shaping(simparams, farfield_samples):
         E_grid = dynamic_to_grid(locations, E, location_sizes, plane_size, step_size, farfield_samples)
         E_ff = []
     return E_grid, E_ff
-
-def martin98(locations, E_incident, permittivity, location_sizes, wavelength, step_size):
-    """
-    Basic implementation of the algorithm as proposed in [Olivier J. F. Martin
-    and Nicolas B. Piller, Electromagnetic scattering in polarizable 
-    back-grounds].
-
-    Parameters
-    ----------
-    locations : numpy array
-        Array containing the locations where the E field must be evaluated.
-    E_incident : numpy array
-        Array containing the value of the incident E field at each location.
-    permittivity : numpy array
-        Array containing the permittivity at each location.
-    location_sizes : numpy array
-        Array containing the size of the samples at each location.
-    wavelength : float
-        Wavelength of the incident wave.
-    step_size : float
-        Minimal distance between samples.
-
-    Returns
-    -------
-    E_r : numpy array
-        Scattered E field at each location.
-
-    """
-    # Find number of locations
-    nloc = np.shape(locations)[0]
-    # Relative permittivity of background
-    epsilon_B = 1
-    # Wave number
-    #k_0 = 2*np.pi*frequency/speed_of_light
-    k_0 = 2*np.pi/wavelength
-    k_rho = k_0*np.sqrt(epsilon_B)
-    # Calculate distance between all points in the plane
-    varrho = squareform(pdist(locations, 'euclidean'))
-    # Calculate G matrix
-    G_condensed = 1j/4*hankel1(0,k_rho*varrho)
-    # Convert condensed G matrix to square form
-    #G = squareform(G_condensed)
-    G = G_condensed
-    # Volume of each location
-    V_mesh = np.square(location_sizes*step_size)
-    # Self contribution to the electric field
-    R_eff = np.sqrt(V_mesh/np.pi) #Effective radius
-    beta = 1 # No coupling between TE and TM polarizations
-    gamma = R_eff/k_rho*hankel1(1, k_rho*R_eff) + 2j/(np.pi*np.square(k_rho))
-    M = 1j*np.pi/2*beta*gamma
-    # Set diagonal of G matrix to 0
-    np.fill_diagonal(G, M/V_mesh)
-    
-    # Difference between background permittivity and permittivity at a specific
-    # location
-    Delta_epsilon = permittivity - epsilon_B
-    
-    # Total E field (vector)
-    E_r = np.linalg.inv(np.identity(nloc) - k_0**2 * G @ np.diag(Delta_epsilon*V_mesh)) @ E_incident
-    return E_r
