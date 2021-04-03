5635625f096be93297c924d0afe5ca7f0f494ee0
Jules-Hilkens <64689330+Jules-Hilkens@users.noreply.github.com>
2021-04-03 16:30:22 +0200

    Comments plus changes for farfield

--- validation/validate_with_TEcil.py
+++ validation/validate_with_TEcil.py
@@ -18,12 +18,12 @@ import matplotlib.pyplot as plt
 from scipy.constants import epsilon_0, mu_0
 
 # Create epsilon plane
-simulation_size = (52,52)
-total_size = 150;
+simulation_size = (100,100) #number of samples in x- and y-direction, respectively
+total_size = 500; #size of grid, meters
 step_size = total_size/simulation_size[0]  #meters
 
 # Circle in middle
-circle_diameter = total_size/3 #meters
+circle_diameter = total_size/10 #meters
 circle_permittivity = 4.7 #relative (glass)
 epsilon_circle = plane_with_circle(simulation_size, step_size, circle_diameter, circle_permittivity)
 
@@ -31,10 +31,10 @@ epsilon_circle = plane_with_circle(simulation_size, step_size, circle_diameter, 
 show_plane(np.real(epsilon_circle), step_size, title="Plane on which the field is incident",plottype='epsilon')
 
 # Define input wave properties
-frequency = 1e6
-wavelength = speed_of_light/frequency
-theta_i = -45;
-input_angle = theta_i*np.pi/180
+frequency = 1e6 #Hz
+wavelength = speed_of_light/frequency #meters
+theta_i = 0; #degree
+input_angle = theta_i*np.pi/180 #radians
 farfield_samples = 120
 
 #STATIC GRID
@@ -48,9 +48,9 @@ simparams = {
     }
 
 # Compute E-field using domain_integral_equation
-start_algorithm = timer()
-E_field, E_ff = domain_integral_equation(simparams)
-end_algorithm = timer()
+start_algorithm = timer() #Start timer 
+E_field, E_ff = domain_integral_equation(simparams) 
+end_algorithm = timer() #End timer, can see how long domain_integral_equation took
 print("Solution found with static algorithm in {} seconds".format(end_algorithm-start_algorithm))
 
 # Show the calculated E field
@@ -58,7 +58,7 @@ show_plane(np.absolute(E_field), step_size, title="E field calculated with stati
 
 #DYNAMIC GRID
 # Store necessary variables into dictionary for E-field computation
-size_limits = [0, 2*circle_diameter, 4*circle_diameter]
+size_limits = [0, 2*circle_diameter, 4*circle_diameter] #Location where area of sample increases  
 simparams = {
     'simulation_size': simulation_size,
     'step_size': step_size,
@@ -80,21 +80,22 @@ print("Solution found with dynamic algorithm in {} seconds".format(end_algorithm
 show_plane(np.absolute(E_grid), step_size, title="E field calculated with dynamic algorithm",plottype='fieldnorm')
 
 #REFERENCE FIELD
-# TEcil expects different simparams, so create new dictionary
-locations = (np.array(list(np.ndindex(simulation_size[0],simulation_size[1])))+0.5)*step_size
-xpoints = locations[:,0] - simulation_size[0]*step_size/2
-ypoints = locations[:,1] - simulation_size[1]*step_size/2
+locations = (np.array(list(np.ndindex(simulation_size[0],simulation_size[1])))+0.5)*step_size #Creates an array of locations
+xpoints = locations[:,0] - simulation_size[0]*step_size/2 #Creates an array of x-coordinates center around the origin
+ypoints = locations[:,1] - simulation_size[1]*step_size/2 #Creates an array of y-coordinates center around the origin
 
-if farfield_samples != 0:
+#Calculate farfield used for validation
+if farfield_samples != 0: 
     ff_distance = 10*round(wavelength) #Farfield calculated at this distance from cylinder
-    ff_angle = np.linspace(0, 2*np.pi, farfield_samples, endpoint=False) #Starting angle in radians 45 degrees from incident
-    loc_ff = np.array([np.cos(ff_angle), np.sin(ff_angle)]).T*ff_distance
+    ff_angle = np.linspace(0, 2*np.pi, farfield_samples, endpoint=False) #Angles at which farfield samples will be calculated 
+    loc_ff = np.array([np.cos(ff_angle+np.pi/2), np.sin(ff_angle+np.pi/2)]).T*ff_distance #Locations of the farfield
     loc_ff = np.array([loc_ff[:,0]+simulation_size[0]/2*step_size, loc_ff[:,1]+simulation_size[0]/2*step_size]).T #Shift locations around center of simulation plane
-    x_ff = loc_ff[:,0]
-    y_ff = loc_ff[:,1]
-    xpoints = np.append(xpoints,x_ff)
-    ypoints = np.append(ypoints,y_ff)
+    x_ff = loc_ff[:,0] #x-coordinate of farfield
+    y_ff = loc_ff[:,1] #y-coordinate of farfield
+    xpoints = np.append(xpoints,x_ff) #Adding farfield x-coordinates to array of x-coordinates
+    ypoints = np.append(ypoints,y_ff) #Adding farfield y-coordinates to array of x-coordinates
 
+# TEcil expects different simparams, so create new dictionary
 simparams = {
     'frequency': frequency,
     'radius': circle_diameter/2,
@@ -111,16 +112,17 @@ _, _, E_fieldval, E_inval = Analytical_2D_TE(simparams)
 end_analytical = timer()
 print("Analytical solution found in {} seconds".format(end_analytical-start_analytical))
 
-if farfield_samples != 0 :
-    E_fieldval_ff  = E_fieldval[-farfield_samples:]
-    E_fieldval_dyn = E_fieldval[:-farfield_samples]
+# Calculate farfield used for validation
+if farfield_samples != 0 : 
+    E_fieldval_ff  = E_fieldval[-farfield_samples:] #Take farfield samples out of E_fieldval
+    E_fieldval = E_fieldval[:-farfield_samples] #Remove farfield values from E_fieldval
     E_inval = E_inval[:-farfield_samples]
     E_0 = np.sqrt(mu_0/epsilon_0) #Amplitude of incident wave in background medium
     E_fieldval_ff = E_fieldval_ff/E_0 #Normalizing over E_0
 
 # Show the validation E field
-E_fieldval = np.reshape(E_fieldval, simulation_size, order='C')
-E_fieldval = E_fieldval.T
+E_fieldval = np.reshape(E_fieldval, simulation_size, order='C') #Convert array back to matrix
+E_fieldval = E_fieldval.T #Change x and y values
 show_plane(np.absolute(E_fieldval), step_size, title="E field of analytical solution on static grid")
 
 #ERROR CALCULATION
@@ -148,31 +150,16 @@ show_plane(E_griderror, step_size, title="Error between analytical and dynamic a
 #E_in = dynamic_to_grid(locations,E_inval,location_sizes,simulation_size,step_size, farfield_samples).T
 #show_plane(np.real(E_in.T), step_size, title="Validation incident field")
 
-#FARFIELD CALCULATION AND VALIDATION
-# Calculate locations farfield for validation
-
-# Reference locations farfield
-
-# simparams = {
-#     'frequency': frequency,
-#     'radius': circle_diameter/2,
-#     'epsilon_r': circle_permittivity,
-#     'incident_angle': input_angle,
-#     'modes': 50, #used in jupyter notebook example
-#     'evaluation_points_x': x_ff,
-#     'evaluation_points_y': y_ff
-#     }
-# _, _, E_fieldval_ff, E_inval_ff = Analytical_2D_TE(simparams)
-# E_0 = np.sqrt(mu_0/epsilon_0) #Amplitude of incident wave in background medium
-# E_fieldval_ff = E_ff/E_0
-#show_plane_ff(np.absolute(E_ff), loc_ff, ff_angle, ff_distance, title="Locations farfield of algorithm solution")
-plt.figure()
-plt.plot(ff_angle,np.absolute(E_ff)) #Line plot
-plt.scatter(ff_angle,np.absolute(E_ff), color='c', label='Algorithm') #Scatter plot
-plt.plot(ff_angle,np.absolute(E_fieldval_ff)) #Turn on if maximum error is desired
-plt.scatter(ff_angle,np.absolute(E_fieldval_ff), color='gold', label='Analytical solution')
-plt.grid(True, which='both')
-plt.xlabel("Angle [rad]")
-plt.ylabel("Normalized E-field magnitude")
-plt.title("Normalized farfield values at %i m from cylinder" %ff_distance)
-plt.legend()
+#FARFIELD VALIDATION
+# Plot the farfield samples from the algorithm against farfield samples from analytical solution
+if farfield_samples != 0:
+    plt.figure()
+    plt.plot(ff_angle,np.absolute(E_ff)) #Line plot
+    plt.scatter(ff_angle,np.absolute(E_ff), color='c', label='Algorithm') #Scatter plot
+    plt.plot(ff_angle,np.absolute(E_fieldval_ff)) #Turn on if maximum error is desired
+    plt.scatter(ff_angle,np.absolute(E_fieldval_ff), color='gold', label='Analytical solution')
+    plt.grid(True, which='both')
+    plt.xlabel("Angle [rad]")
+    plt.ylabel("Normalized E-field magnitude")
+    plt.title("Normalized farfield values at %i m from cylinder" %ff_distance)
+    plt.legend()
